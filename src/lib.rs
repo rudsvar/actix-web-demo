151fe78fccef88892db5509ee55283f4a698c9d5
Rudi Blaha Svartveit <rudsvar@gmail.com>
2021-12-03 20:27:52 +0100

    Add dummy subscription endpoint with tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,27 @@
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
 #[actix_web::get("/health_check")]
-pub async fn health_check(_: HttpRequest) -> impl Responder {
+pub async fn health_check() -> impl Responder {
     log::info!("I'm fine!");
     HttpResponse::Ok()
 }
 
-pub async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+#[derive(Debug, Deserialize)]
+pub struct FormData {
+    name: String,
+    email: String,
+}
+
+#[actix_web::post("/subscriptions")]
+pub async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
 }
 
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .service(health_check)
-            .route("/hello/", web::get().to(greet))
-            .route("/hello/{name}", web::get().to(greet))
-    })
-    .listen(listener)?
-    .run();
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
+        .listen(listener)?
+        .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,13 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = actix_web_demo::run(listener).expect("failed to start server");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange
@@ -18,10 +26,50 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = actix_web_demo::run(listener).expect("failed to start server");
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+#[actix_rt::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    // Act
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
