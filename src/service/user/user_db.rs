2244686d99207a3bbcb557d4ebe30ffadaa3f548
Rudi Blaha Svartveit <rudsvar@gmail.com>
2022-04-29 23:04:29 +0200

    Use PgExecutor instead of explicit PgPool

--- src/service/user/user_db.rs
+++ src/service/user/user_db.rs
@@ -4,11 +4,11 @@ use super::user_model::NewUser;
 use crate::{
     error::DbError,
     service::user::user_model::{HashedPassword, User},
-    DbPool,
 };
+use sqlx::PgExecutor;
 
 /// Store a new user in the database.
-pub async fn store_user(pool: &DbPool, new_user: &NewUser) -> Result<User, DbError> {
+pub async fn store_user(conn: impl PgExecutor<'_>, new_user: &NewUser) -> Result<User, DbError> {
     tracing::info!("Storing user with name {}", &new_user.name);
     let hashed_password = HashedPassword::new(new_user.password.clone());
     let user = sqlx::query_as!(
@@ -21,38 +21,42 @@ pub async fn store_user(pool: &DbPool, new_user: &NewUser) -> Result<User, DbErr
         &new_user.name,
         &hashed_password.hashed_password()
     )
-    .fetch_one(pool)
+    .fetch_one(conn)
     .await?;
     Ok(user)
 }
 
 /// Fetch a user from the database by id.
-pub async fn fetch_user_by_id(pool: &DbPool, id: &i32) -> Result<User, DbError> {
+pub async fn fetch_user_by_id(conn: impl PgExecutor<'_>, id: &i32) -> Result<User, DbError> {
     let user = sqlx::query_as!(
         User,
         r#"SELECT id, name, password as "password: HashedPassword", created_at FROM users WHERE id = $1"#,
         id
     )
-    .fetch_one(pool)
+    .fetch_one(conn)
     .await?;
 
     Ok(user)
 }
 
 /// List all users.
-pub async fn fetch_all_users(pool: &DbPool) -> Result<Vec<User>, DbError> {
+pub async fn fetch_all_users(conn: impl PgExecutor<'_>) -> Result<Vec<User>, DbError> {
     let users = sqlx::query_as!(
         User,
         r#"SELECT id, name, password as "password: HashedPassword", created_at FROM users"#,
     )
-    .fetch_all(pool)
+    .fetch_all(conn)
     .await?;
 
     Ok(users)
 }
 
 /// Verify a password.
-pub async fn verify_password(pool: &DbPool, id: &i32, password: &str) -> Result<bool, DbError> {
-    let user = fetch_user_by_id(pool, id).await?;
+pub async fn verify_password(
+    conn: impl PgExecutor<'_>,
+    id: &i32,
+    password: &str,
+) -> Result<bool, DbError> {
+    let user = fetch_user_by_id(conn, id).await?;
     Ok(user.password.verify(password))
 }
