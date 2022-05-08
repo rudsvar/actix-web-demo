4985f2fe7f8197d199253a92ed1b78520f17f381
Rudi Blaha Svartveit <rudsvar@gmail.com>
2022-05-08 13:48:10 +0200

    Experimental repository changes

--- src/repository.rs
+++ src/repository.rs
@@ -2,13 +2,13 @@ use async_trait::async_trait;
 
 pub mod account_repository;
 
-#[async_trait]
+#[async_trait(?Send)]
 pub trait Repository {
     type Id;
     type Store;
     type Load;
 
-    async fn fetch(&self, id: Self::Id) -> Self::Load;
+    async fn fetch(&mut self, id: Self::Id) -> Self::Load;
     async fn store(&self, data: Self::Store) -> Self::Load;
     async fn update(&self, data: Self::Store) -> Self::Load;
     async fn delete(&self, id: Self::Id) -> i32;
--- src/repository/account_repository.rs
+++ src/repository/account_repository.rs
@@ -7,22 +7,25 @@ use crate::{
 use async_trait::async_trait;
 use sqlx::{PgExecutor, PgPool, Postgres, Transaction};
 
-pub struct AccountRepository(PgPool);
+pub struct AccountRepository<T>(T);
 
-#[async_trait]
-impl Repository for AccountRepository {
+#[async_trait(?Send)]
+impl<T> Repository for AccountRepository<T>
+where
+    for<'a> &'a mut T: PgExecutor<'static>,
+{
     type Id = (i32, i32);
     type Store = NewAccount;
     type Load = Result<Account, DbError>;
 
-    async fn fetch<'b>(&'b self, id: Self::Id) -> Self::Load {
+    async fn fetch<'b>(&'b mut self, id: Self::Id) -> Self::Load {
         let result = sqlx::query_as!(
             Account,
             r#"SELECT * FROM accounts WHERE id = $1 AND owner_id = $2"#,
             id.0,
             id.1,
         )
-        .fetch_one(&self.0)
+        .fetch_one(&mut self.0)
         .await
         .map_err(DbError::from)?;
         Ok(result)
@@ -42,10 +45,10 @@ impl Repository for AccountRepository {
 }
 
 async fn foo<'a>(e: impl PgExecutor<'static>) {
-    sqlx::query("select * from users").execute(e).await;
+    // sqlx::query("select * from users").execute(e).await;
     let x: DbPool = todo!();
-    let tx: Transaction<'_, Postgres> = x.begin().await.unwrap();
-    foo(&mut tx).await;
-    // let ar = AccountRepository(e);
-    // let account = ar.fetch((0, 0));
+    let tx: Transaction<'static, Postgres> = x.begin().await.unwrap();
+    // foo(&mut tx).await;
+    let ar = AccountRepository(tx);
+    let account = ar.fetch((0, 0)).await;
 }
