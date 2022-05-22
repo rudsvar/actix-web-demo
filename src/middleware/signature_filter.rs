7abc79f1e4716e563337aaee19652b0d6be6a4c6
Rudi Blaha Svartveit <rudsvar@gmail.com>
2022-05-22 18:19:08 +0200

    Improve error handling of signature validation

--- src/middleware/signature_filter.rs
+++ src/middleware/signature_filter.rs
@@ -15,6 +15,49 @@ pub struct SignatureFilterService<S> {
     service: Rc<S>,
 }
 
+fn validate_signature(req: &ServiceRequest) -> Result<(), HttpResponse> {
+    let header_map = req.headers();
+    tracing::info!("Getting authorization header");
+    let signature_header = header_map
+        .get("Authorization")
+        .ok_or_else(HttpResponse::Unauthorized)?
+        .to_str()
+        .map_err(|_| HttpResponse::Unauthorized())?;
+    let signature_header: SignatureHeader = signature_header
+        .parse()
+        .map_err(|_| HttpResponse::Unauthorized())?;
+    tracing::info!("Got signature header {}", signature_header);
+
+    // Collect headers to sign
+    let mut headers_to_sign: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mandatory_headers = vec![];
+    for &h in &mandatory_headers {
+        let values: Vec<&str> = header_map.get_all(h).map(|h| h.to_str().unwrap()).collect();
+        headers_to_sign.insert(h, values);
+    }
+    let request_target = format!("{} {}", req.method(), req.uri());
+    headers_to_sign.insert("(request-target)", vec![request_target.as_str()]);
+
+    // Compute the expected signature string
+    let signature_string = signature::signature_string(&mandatory_headers, &headers_to_sign);
+    tracing::info!("Verifying signature string {}", signature_string);
+
+    // Get the provided signature
+    let provided_signature = base64::decode(signature_header.signature()).unwrap();
+
+    // Decrypt provided signature with client's public key, and make sure it matches the signature string
+    let public_key =
+        signature::load_public_key(&format!("./keys/{}.pem", signature_header.key_id()));
+    let verified = signature::verify(signature_string.as_bytes(), &provided_signature, public_key);
+    if verified {
+        tracing::info!("Signature validation succeeded");
+        Ok(())
+    } else {
+        tracing::info!("Signature validation failed");
+        Err(HttpResponse::Unauthorized().finish())
+    }
+}
+
 impl<S> Service<ServiceRequest> for SignatureFilterService<S>
 where
     S: Service<ServiceRequest, Response = ServiceResponse<BoxBody>>,
@@ -28,52 +71,15 @@ where
     actix_web::dev::forward_ready!(service);
 
     fn call(&self, req: ServiceRequest) -> Self::Future {
-        let header_map = req.headers();
-
-        // Get authorization header
-        tracing::info!("Getting authorization header");
-        let signature_header = header_map.get("Authorization").unwrap().to_str().unwrap();
-        let signature_header: SignatureHeader = signature_header.parse().unwrap();
-        tracing::info!("Got signature header {}", signature_header);
-
-        // Collect headers to sign
-        let mut headers_to_sign: HashMap<&str, Vec<&str>> = HashMap::new();
-        let mandatory_headers = vec![];
-        for &h in &mandatory_headers {
-            let values: Vec<&str> = header_map.get_all(h).map(|h| h.to_str().unwrap()).collect();
-            headers_to_sign.insert(h, values);
-        }
-        let request_target = format!("{} {}", req.method(), req.uri());
-        headers_to_sign.insert("(request-target)", vec![request_target.as_str()]);
-
-        // Compute the expected signature string
-        let signature_string = signature::signature_string(&mandatory_headers, &headers_to_sign);
-        tracing::info!("Verifying signature string {}", signature_string);
-
-        // Get the provided signature
-        let provided_signature = base64::decode(signature_header.signature()).unwrap();
-
-        // Decrypt provided signature with client's public key, and make sure it matches the signature string
-        let public_key =
-            signature::load_public_key(&format!("./keys/{}.pem", signature_header.key_id()));
-        let verified =
-            signature::verify(signature_string.as_bytes(), &provided_signature, public_key);
-        if !verified {
-            panic!("failed to validate signature")
-        }
-
-        tracing::info!("Signature verified");
-
-        // Call inner service if signature validation succeeded
-        // let res = self.service.call(req);
+        let validation_result = validate_signature(&req);
         let service = Rc::clone(&self.service);
         Box::pin(async move {
-            if verified {
-                let resp = service.call(req).await?;
-                Ok(resp.map_into_boxed_body())
-            } else {
-                let resp = HttpResponse::Unauthorized().finish();
-                Ok(req.into_response(resp).map_into_boxed_body())
+            match validation_result {
+                Ok(()) => {
+                    let resp = service.call(req).await?;
+                    Ok(resp.map_into_boxed_body())
+                }
+                Err(status) => Ok(req.into_response(status).map_into_boxed_body()),
             }
         })
     }
