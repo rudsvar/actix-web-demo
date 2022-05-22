dfafbbf191a96b47cf62d71d6a7bb812f318331a
Rudi Blaha Svartveit <rudsvar@gmail.com>
2022-05-22 19:21:00 +0200

    Add tests that signs with wrong private key

--- tests/service/signature_test.rs
+++ tests/service/signature_test.rs
@@ -74,6 +74,40 @@ async fn edited_signed_request_fails() {
 }
 
 #[actix_rt::test]
+async fn signed_with_wrong_key_fails() {
+    let app = spawn_test_app().await;
+    let client = reqwest::Client::new();
+
+    let headers_to_sign = vec!["(request-target)"];
+
+    let private_key = signature::load_private_key("./tests/wrong-test-signing-key.pem");
+    let mut headers = HashMap::new();
+    headers.insert("(request-target)", vec!["get /signature"]);
+    let signature_string = signature::signature_string(&headers_to_sign, &headers);
+    let signature = signature::sign(signature_string.as_bytes(), private_key);
+    let base64_signature = base64::encode(&signature);
+    let signature_header = SignatureHeader::new(
+        "test".to_string(),
+        "ecdsa-sha256".to_string(),
+        headers_to_sign
+            .iter()
+            .cloned()
+            .map(|s| s.to_string())
+            .collect(),
+        base64_signature,
+    );
+
+    let response = client
+        .get(format!("{}/signature", app.address()))
+        .header("Authorization", signature_header.to_string())
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    assert_eq!(StatusCode::UNAUTHORIZED, response.status());
+}
+
+#[actix_rt::test]
 async fn unsigned_request_fails() {
     let app = spawn_test_app().await;
     let client = reqwest::Client::new();
