1e5ec5641f44f2bc054345c70e4e6ee97c7e5aac
Rudi Blaha Svartveit <rudsvar@gmail.com>
2022-05-23 22:48:25 +0200

    Run cargo fmt

--- tests/service/mod.rs
+++ tests/service/mod.rs
@@ -3,9 +3,9 @@ use crate::common::TestApp;
 mod account_test;
 mod auth_test;
 mod security_test;
+mod signature_test;
 pub mod transfer_test;
 mod user_test;
-mod signature_test;
 
 pub async fn authenticate(app: &TestApp, username: &str, password: &str) -> String {
     let client = reqwest::Client::new();
