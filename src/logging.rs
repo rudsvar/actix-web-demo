24f3ad832db2394db9ddc3cef3c598ff4f05f736
Rudi Blaha Svartveit <rudsvar@gmail.com>
2022-08-02 18:57:23 +0200

    Add support for more log formats

--- src/logging.rs
+++ src/logging.rs
@@ -4,16 +4,28 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilte
 
 /// Initialize logging facilitites.
 pub fn init_logging() {
+    let log_format = std::env::var("LOG_FORMAT").ok();
+    let log_format = log_format.as_deref();
     let console_layer = console_subscriber::spawn();
     let registry = tracing_subscriber::registry().with(console_layer);
-    if cfg!(debug_assertions) {
-        let fmt_layer = tracing_subscriber::fmt::layer().with_filter(EnvFilter::from_default_env());
-        registry.with(fmt_layer).init();
-    } else {
-        let json_storage_layer = JsonStorageLayer;
-        let bunyan_layer =
-            BunyanFormattingLayer::new("actix-web-demo".to_string(), std::io::stdout)
+    match log_format {
+        Some("bunyan") => {
+            let json_storage_layer = JsonStorageLayer;
+            let bunyan_layer =
+                BunyanFormattingLayer::new("actix-web-demo".to_string(), std::io::stdout)
+                    .with_filter(EnvFilter::from_default_env());
+            registry.with(json_storage_layer).with(bunyan_layer).init();
+        }
+        Some("json") => {
+            let json_layer = tracing_subscriber::fmt::layer()
+                .json()
                 .with_filter(EnvFilter::from_default_env());
-        registry.with(json_storage_layer).with(bunyan_layer).init();
+            registry.with(json_layer).init();
+        }
+        _ => {
+            let fmt_layer =
+                tracing_subscriber::fmt::layer().with_filter(EnvFilter::from_default_env());
+            registry.with(fmt_layer).init();
+        }
     }
 }
