bb689b6866c313ce519daed4bc74dfa29fb43763
Rudi Blaha Svartveit <rudsvar@gmail.com>
2022-09-18 12:41:14 +0200

    Run cargo fmt

--- src/infra/middleware/mod.rs
+++ src/infra/middleware/mod.rs
@@ -2,14 +2,14 @@
 
 pub mod authentication_filter;
 pub mod authenticator;
-pub mod header_setter;
 pub mod digest_filter;
+pub mod header_setter;
 pub mod request_logger;
 pub mod signature_filter;
 
 pub use authentication_filter::AuthenticationFilter;
 pub use authenticator::Authenticator;
-pub use header_setter::HeaderSetter;
 pub use digest_filter::DigestFilter;
+pub use header_setter::HeaderSetter;
 pub use request_logger::RequestLogger;
 pub use signature_filter::SignatureFilter;
--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -1,7 +1,7 @@
 //! Database interaction.
 
 pub mod account_repository;
-pub mod transfer_repository;
-pub mod user_repository;
 pub mod audit_log_repository;
 pub mod request_repository;
+pub mod transfer_repository;
+pub mod user_repository;
